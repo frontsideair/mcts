79f8969140cd6ea5a1df98bb09ad1dba168757c3
Fatih Altinok <photonia@gmail.com>
2018-09-14 22:42:15 +0300

    Print stats of immediate leaves of Monte Carlo tree

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,9 @@
 import           MCTS
 import           Hamiltonicity
+import           TicTacToe
 
 main :: IO ()
 main = do
   tree <- runMCTSTimeout 10 initialGameTree :: IO (GameTree Hamiltonicity)
+  print $ stats tree
   print $ robustChild tree
