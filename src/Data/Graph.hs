4f5f5868e5e01777929d309971caeb9374aa0a30
Fatih Altinok <photonia@gmail.com>
2018-11-05 23:00:22 +0300

    Move rest of Graph to Graph.Internal

--- src/Data/Graph.hs
+++ src/Data/Graph.hs
@@ -14,37 +14,3 @@ import qualified Data.Set                      as S
 import           Data.Foldable                  ( any )
 
 import           Data.Graph.Internal
-
--- uncoloredEdges is the set of edges unclaimed by maker or breaker
--- makerColoredEdges is the set of edges claimed by maker
--- winningHamCycles is the set that contains the cycles made of uncolored and maker colored edges
--- (uncoloredEdges, makerColoredEdges, winningHamCycles)
-type Graph = (Set Edge, Set Edge, Set (Set Edge))
-
-complete :: Int -> Graph
-complete n = (uncoloredEdges, makerColored, winningPaths)
- where
-  uncoloredEdges = S.fromList [ edge u v | u <- [1 .. n], v <- [u + 1 .. n] ]
-  makerColored   = S.empty
-  winningPaths   = cycles n
-
-colorMaker :: Edge -> Graph -> Graph
-colorMaker edge@(u, v) (uncoloredEdges, makerColored, winningPaths) =
-  (uncoloredEdges', makerColored', winningPaths)
- where
-  uncoloredEdges' = S.delete edge uncoloredEdges
-  makerColored'   = S.insert edge makerColored
-
-colorBreaker :: Edge -> Graph -> Graph
-colorBreaker edge@(u, v) (uncoloredEdges, makerColored, winningPaths) =
-  (uncoloredEdges', makerColored, winningPaths')
- where
-  uncoloredEdges' = S.delete edge uncoloredEdges
-  winningPaths'   = S.filter (S.notMember edge) winningPaths
-
-uncoloredEdges :: Graph -> [Edge]
-uncoloredEdges (uncolored, _, _) = S.toList uncolored
-
-hasHamiltonianCycle :: Graph -> Bool
-hasHamiltonianCycle (_, makerColored, winningPaths) =
-  any (`S.isSubsetOf` makerColored) winningPaths
--- src/Data/Graph/Internal.hs
+++ src/Data/Graph/Internal.hs
@@ -3,12 +3,25 @@ module Data.Graph.Internal where
 import           Data.Set                       ( Set )
 import qualified Data.Set                      as S
 
+-- uncoloredEdges is the set of edges unclaimed by maker or breaker
+-- makerColoredEdges is the set of edges claimed by maker
+-- winningHamCycles is the set that contains the cycles made of uncolored and maker colored edges
+-- (uncoloredEdges, makerColoredEdges, winningHamCycles)
+type Graph = (Set Edge, Set Edge, Set (Set Edge))
+
 type Vertex = Int
 
 type Edge = (Vertex, Vertex)
 
 edge :: Vertex -> Vertex -> Edge
 edge u v = if u < v then (u, v) else (v, u)
+
+complete :: Int -> Graph
+complete n = (uncoloredEdges, makerColored, winningPaths)
+ where
+  uncoloredEdges = S.fromList [ edge u v | u <- [1 .. n], v <- [u + 1 .. n] ]
+  makerColored   = S.empty
+  winningPaths   = cycles n
 
 rotate :: [a] -> [a]
 rotate xs = take (length xs) $ (tail . cycle) xs
@@ -20,3 +33,27 @@ cycles n = S.fromList $ S.fromList . pathToCycle <$> cyclicPaths
   cyclicPaths = iterate grow [[1]] !! (n - 1)
   grow paths =
     [ vertex : path | path <- paths, vertex <- [1 .. n], vertex `notElem` path ]
+
+uncoloredEdges :: Graph -> [Edge]
+uncoloredEdges (uncolored, _, _) = S.toList uncolored
+
+winningPaths :: Graph -> Set (Set Edge)
+winningPaths (_, _, winningPaths) = winningPaths
+
+colorMaker :: Edge -> Graph -> Graph
+colorMaker edge@(u, v) (uncoloredEdges, makerColored, winningPaths) =
+  (uncoloredEdges', makerColored', winningPaths)
+ where
+  uncoloredEdges' = S.delete edge uncoloredEdges
+  makerColored'   = S.insert edge makerColored
+
+colorBreaker :: Edge -> Graph -> Graph
+colorBreaker edge@(u, v) (uncoloredEdges, makerColored, winningPaths) =
+  (uncoloredEdges', makerColored, winningPaths')
+ where
+  uncoloredEdges' = S.delete edge uncoloredEdges
+  winningPaths'   = S.filter (S.notMember edge) winningPaths
+
+hasHamiltonianCycle :: Graph -> Bool
+hasHamiltonianCycle (_, makerColored, winningPaths) =
+  any (`S.isSubsetOf` makerColored) winningPaths
