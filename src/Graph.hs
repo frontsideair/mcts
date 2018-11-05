f6327009d1f9ca847a9bcfac8d4ca17e47c1d7a6
Fatih Altinok <photonia@gmail.com>
2018-11-05 22:28:01 +0300

    Split Graph logic to be tested to Internal module

--- src/Graph.hs
+++ src/Graph.hs
@@ -13,18 +13,13 @@ import           Data.Set                       ( Set )
 import qualified Data.Set                      as S
 import           Data.Foldable                  ( any )
 
+import           Graph.Internal
+
 -- uncoloredEdges is the set of edges unclaimed by maker or breaker
 -- makerColoredEdges is the set of edges claimed by maker
 -- winningHamCycles is the set that contains the cycles made of uncolored and maker colored edges
 -- (uncoloredEdges, makerColoredEdges, winningHamCycles)
 type Graph = (Set Edge, Set Edge, Set (Set Edge))
-
-type Vertex = Int
-
-type Edge = (Vertex, Vertex)
-
-edge :: Vertex -> Vertex -> Edge
-edge u v = if u < v then (u, v) else (v, u)
 
 complete :: Int -> Graph
 complete n = (uncoloredEdges, makerColored, winningPaths)
@@ -32,17 +27,6 @@ complete n = (uncoloredEdges, makerColored, winningPaths)
   uncoloredEdges = S.fromList [ edge u v | u <- [1 .. n], v <- [u + 1 .. n] ]
   makerColored   = S.empty
   winningPaths   = cycles n
-
-rotate :: [a] -> [a]
-rotate = tail . cycle
-
-cycles :: Int -> Set (Set Edge)
-cycles n = S.fromList $ S.fromList . pathToCycle <$> cyclicPaths
- where
-  pathToCycle xs = zipWith edge xs (rotate xs)
-  cyclicPaths = iterate grow [[1]] !! (n - 1)
-  grow paths =
-    [ vertex : path | path <- paths, vertex <- [1 .. n], vertex `notElem` path ]
 
 colorMaker :: Edge -> Graph -> Graph
 colorMaker edge@(u, v) (uncoloredEdges, makerColored, winningPaths) =
---
+++ src/Graph/Internal.hs
@@ -0,0 +1,22 @@
+module Graph.Internal where
+
+import           Data.Set                       ( Set )
+import qualified Data.Set                      as S
+
+type Vertex = Int
+
+type Edge = (Vertex, Vertex)
+
+edge :: Vertex -> Vertex -> Edge
+edge u v = if u < v then (u, v) else (v, u)
+
+rotate :: [a] -> [a]
+rotate xs = take (length xs) $ (tail . cycle) xs
+
+cycles :: Int -> Set (Set Edge)
+cycles n = S.fromList $ S.fromList . pathToCycle <$> cyclicPaths
+ where
+  pathToCycle xs = zipWith edge xs (rotate xs)
+  cyclicPaths = iterate grow [[1]] !! (n - 1)
+  grow paths =
+    [ vertex : path | path <- paths, vertex <- [1 .. n], vertex `notElem` path ]
