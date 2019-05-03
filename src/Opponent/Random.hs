922c0587097197571edf08a5c4efbffaf4e7929a
Fatih Altinok <photonia@gmail.com>
2019-05-03 14:10:46 +0300

    Implement a channel based random player

--- src/Opponent/Random.hs
+++ src/Opponent/Random.hs
@@ -1,14 +1,32 @@
 {-# LANGUAGE NamedFieldPuns #-}
 module Opponent.Random
-  ( randomPlay
+  ( randomPlayChan
   )
 where
 
-import           Game                           ( Game(..) )
+import           Game                           ( Game(..)
+                                                , Message(Start, Move)
+                                                , AIPlayer
+                                                )
 import           Data.Random                    ( sample
                                                 , randomElement
                                                 )
+import           Control.Concurrent.Process     ( readProcess
+                                                , writeProcess
+                                                )
 
-randomPlay :: Game g m p -> Int -> g -> IO m
-randomPlay Game { legalMoves } _ game =
-  sample $ randomElement $ legalMoves game
+randomPlay :: Game g m p -> g -> IO m
+randomPlay Game { legalMoves } game = sample $ randomElement $ legalMoves game
+
+randomPlayChan :: (Ord m, Eq p) => AIPlayer g m p
+randomPlayChan g@Game { play, initialGame, legalMoves } process = helper
+  initialGame
+ where
+  helper game = do
+    message <- readProcess process
+    let game' = case message of
+          Start  -> initialGame
+          Move m -> play m game
+    move <- randomPlay g game
+    writeProcess process (Move move)
+    helper (play move game')
